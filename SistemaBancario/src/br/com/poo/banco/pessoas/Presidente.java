7c3feb359bdf09d3a737ba8ac0d727cefeb481b4
guilh <guilhermeborde99@gmail.com>
2023-09-04 16:42:22 -0300

    feat: Adição de atributos na classe banco.pessoas

--- SistemaBancario/src/br/com/poo/banco/pessoas/Presidente.java
+++ SistemaBancario/src/br/com/poo/banco/pessoas/Presidente.java
@@ -1,5 +1,23 @@
 package br.com.poo.banco.pessoas;
 
-public class Presidente {
+import java.util.Date;
 
+public class Presidente {
+	
+	private String nome;
+	private String cpf;
+	private String email;
+	private Date dataNasc;
+	
+	//construtores
+	
+	public Presidente() {
+		
+	}
+	public Presidente(String nome, String cpf, String email, Date dataNasc) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.dataNasc = dataNasc;
+	}
 }
