1da1ccc608259be284c7ce016823f8b47de088ff
ecard58 <emanuelcardoso58@gmail.com>
2023-09-11 14:15:59 -0300

    fix: correção de JCadFun, JCadCli e JLogin

--- SistemaBancario/src/br/com/poo/banco/views/JCadFun.java
+++ SistemaBancario/src/br/com/poo/banco/views/JCadFun.java
@@ -16,15 +16,18 @@ import javax.swing.SwingConstants;
 import javax.swing.ImageIcon;
 import javax.swing.JFormattedTextField;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JPasswordField;
+import javax.swing.DefaultComboBoxModel;
 
-public class JCadCli extends JFrame {
+public class JCadFun extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField cadNome;
 	private JTextField cadCpf;
 	private JTextField cadDataNasc;
 	private JTextField textField;
+	private JTextField textField_1;
 	private JPasswordField passwordField;
 	private JPasswordField passwordField_1;
 
@@ -35,7 +38,7 @@ public class JCadCli extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					JCadCli frame = new JCadCli();
+					JCadFun frame = new JCadFun();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -47,7 +50,7 @@ public class JCadCli extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public JCadCli() {
+	public JCadFun() {
 		setTitle("STbank");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 600);
@@ -73,10 +76,10 @@ public class JCadCli extends JFrame {
 		cadCadastrar.setBounds(238, 483, 89, 23);
 		contentPane.add(cadCadastrar);
 		
-		JLabel newCadastro = new JLabel("Cadastro - Cliente");
+		JLabel newCadastro = new JLabel("Cadastro - Funcionário");
 		newCadastro.setFont(new Font("Bauhaus 93", Font.BOLD, 36));
 		newCadastro.setForeground(new Color(0, 0, 128));
-		newCadastro.setBounds(232, 11, 332, 44);
+		newCadastro.setBounds(205, 11, 390, 44);
 		contentPane.add(newCadastro);
 		
 		JLabel lblNewLabel = new JLabel("Nome:");
@@ -97,7 +100,7 @@ public class JCadCli extends JFrame {
 			MaskFormatter mascaraCpf = new MaskFormatter("###.###.###-##");
 		    cadCpf = new JFormattedTextField(mascaraCpf);
 		    cadCpf.setColumns(10);
-		    cadCpf.setBounds(54, 145, 200, 32);
+		    cadCpf.setBounds(65, 145, 200, 32);
 		    contentPane.add(cadCpf);				
 		} catch (ParseException e) {
 			e.printStackTrace();
@@ -130,22 +133,42 @@ public class JCadCli extends JFrame {
 		textField.setBounds(65, 212, 450, 32);
 		contentPane.add(textField);
 		
-		JLabel lblNewLabel_2 = new JLabel("Senha:");
+		JLabel lblNewLabel_2 = new JLabel("Cargo:");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel_2.setBounds(10, 333, 66, 23);
+		lblNewLabel_2.setBounds(10, 322, 66, 25);
 		contentPane.add(lblNewLabel_2);
 		
-		JLabel lblNewLabel_3 = new JLabel("Confirme sua senha:");
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Selecione uma opção:", "Gerente", "Diretor", "Presidente"}));
+		comboBox.setBounds(65, 320, 170, 32);
+		contentPane.add(comboBox);
+		
+		JLabel lblNewLabel_3 = new JLabel("Salário:");
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel_3.setBounds(265, 328, 175, 32);
+		lblNewLabel_3.setBounds(10, 380, 66, 23);
 		contentPane.add(lblNewLabel_3);
 		
+		textField_1 = new JTextField();
+		textField_1.setBounds(78, 377, 115, 32);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Senha:");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_4.setBounds(10, 435, 66, 23);
+		contentPane.add(lblNewLabel_4);
+		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(65, 330, 150, 32);
+		passwordField.setBounds(65, 432, 139, 32);
 		contentPane.add(passwordField);
 		
+		JLabel lblNewLabel_5 = new JLabel("Confirme sua senha:");
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_5.setBounds(238, 431, 156, 31);
+		contentPane.add(lblNewLabel_5);
+		
 		passwordField_1 = new JPasswordField();
-		passwordField_1.setBounds(414, 330, 150, 32);
+		passwordField_1.setBounds(388, 432, 139, 32);
 		contentPane.add(passwordField_1);
 		
 		JLabel imagemFundo = new JLabel("");
