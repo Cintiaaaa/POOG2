dbda576e8b819bc1cdacf0739834a22be4269d7b
JuFMacedo <julia_freitasmacedo@hotmail.com>
2023-09-14 14:41:19 -0300

    Fix: Correção de erro e adição de código no sistema bancário

--- SistemaBancario/src/br/com/poo/banco/views/JLogin.java
+++ SistemaBancario/src/br/com/poo/banco/views/JLogin.java
@@ -100,12 +100,11 @@ public class JLogin extends JFrame {
 		JComboBox<String> comboBox = new JComboBox<>();
 		List<PessoaEnum> tipoPessoa = Arrays.asList(PessoaEnum.values());
 		comboBox.addItem("Selecione um tipo:");
-		
-		for(PessoaEnum pe : tipoPessoa)
-		{
+
+		for (PessoaEnum pe : tipoPessoa) {
 			comboBox.addItem(pe.getTipoPessoa());
 		}
-		
+
 		comboBox.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
@@ -139,56 +138,57 @@ public class JLogin extends JFrame {
 				try {
 					cpf = mascaraCpf.valueToString(inputCpf.getText());
 
-				String senha = new String(inputPassword.getPassword());
+					String senha = new String(inputPassword.getPassword());
 
-				Cliente c = Cliente.mapaClientes.get(cpf);
-				Funcionario f = Funcionario.mapaFuncionario.get(cpf);
-				Conta c1 = Conta.mapaContas.get(cpf);
+					Cliente c = Cliente.mapaClientes.get(cpf);
+					Funcionario f = Funcionario.mapaFuncionario.get(cpf);
+					Conta c1 = Conta.mapaContas.get(cpf);
 
-				System.out.println(c.getCpf() + " " + c.getSenha() + " " + cpf + " " + senha);
+					System.out.println(c.getCpf() + " " + c.getSenha() + " " + cpf + " " + senha);
 
-				// validação de cpf e senha
-				if ((c.getCpf().equals(cpf) && c.getSenha().equals(senha))
-						|| (f.getCpf().equals(cpf) && f.getSenha().equals(senha))) {
-					// quem está logando
-					// cliente
-					if (comboBox.getSelectedItem().toString().equalsIgnoreCase(PessoaEnum.CLIENTE.getTipoPessoa())) {
-						// verificação de tipo de conta
-						if (c1.getTipo().equalsIgnoreCase(ContaEnum.CORRENTE.getTipoConta())) {
-							dispose();
-							JContaCorrente jConCor = new JContaCorrente();
-							jConCor.setLocationRelativeTo(jConCor);
-							jConCor.setVisible(true);
-						} else {
-							dispose();
-							JContaPoupanca jConPou = new JContaPoupanca();
-							jConPou.setLocationRelativeTo(jConPou);
-							jConPou.setVisible(true);
+					// validação de cpf e senha
+					if ((c.getCpf().equals(cpf) && c.getSenha().equals(senha))
+							|| (f.getCpf().equals(cpf) && f.getSenha().equals(senha))) {
+						// quem está logando
+						// cliente
+						if (comboBox.getSelectedItem().toString()
+								.equalsIgnoreCase(PessoaEnum.CLIENTE.getTipoPessoa())) {
+							// verificação de tipo de conta
+							if (c1.getTipo().equalsIgnoreCase(ContaEnum.CORRENTE.getTipoConta())) {
+								dispose();
+								JContaCorrente jConCor = new JContaCorrente();
+								jConCor.setLocationRelativeTo(jConCor);
+								jConCor.setVisible(true);
+							} else {
+								dispose();
+								JContaPoupanca jConPou = new JContaPoupanca();
+								jConPou.setLocationRelativeTo(jConPou);
+								jConPou.setVisible(true);
+							}
+						}
+						// gerente
+						else if (comboBox.getSelectedItem().toString().equals(PessoaEnum.GERENTE.getTipoPessoa())) {
+						dispose();
+						JGerente jGer = new JGerente();
+						jGer.setLocationRelativeTo(jGer);
+						jGer.setVisible(true);
+						}
+						// diretor
+						else if (comboBox.getSelectedItem().toString().equals(PessoaEnum.DIRETOR.getTipoPessoa())) {
+						dispose();
+						JDiretor jDir = new JDiretor();
+						jDir.setLocationRelativeTo(jDir);
+						jDir.setVisible(true);
+						}
+						// presidente
+						else if (comboBox.getSelectedItem().toString().equals(PessoaEnum.PRESIDENTE.getTipoPessoa())) {
+						dispose();
+						JPresidente jPres = new JPresidente();
+						jPres.setLocationRelativeTo(jPres);
+						jPres.setVisible(true);
 						}
 					}
-					// gerente
-					else if (comboBox.getSelectedItem().toString().equals(PessoaEnum.GERENTE.getTipoPessoa())) {
-//						dispose();
-//						JGerente jGer = new JGerente();
-//						jGer.setLocationRelativeTo(jGer);
-//						jGer.setVisible(true);
-					}
-					// diretor
-					else if (comboBox.getSelectedItem().toString().equals(PessoaEnum.DIRETOR.getTipoPessoa())) {
-//						dispose();
-//						JGerente jGer = new JGerente();
-//						jGer.setLocationRelativeTo(jGer);
-//						jGer.setVisible(true);
-					}
-					// presidente
-					else if (comboBox.getSelectedItem().toString().equals(PessoaEnum.PRESIDENTE.getTipoPessoa())) {
-//						dispose();
-//						JGerente jGer = new JGerente();
-//						jGer.setLocationRelativeTo(jGer);
-//						jGer.setVisible(true);
-					}
-				}
-				// }
+					// }
 				} catch (ParseException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
@@ -201,7 +201,6 @@ public class JLogin extends JFrame {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
 
 		inputPassword = new JPasswordField();
 		inputPassword.setBounds(120, 243, 368, 35);
